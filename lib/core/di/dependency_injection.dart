edf4b0d14dc7a476c0e8538c4413721b9ba3316a
vahid fotohi <vahidfotohi0@gmail.com>
2025-12-19 18:39:29 +0330

    feat: add dependency injection for Payments, Receipts, Settings, and CRUD UseCases

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -4,13 +4,24 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../data/local/app_database.dart';
 // ایمپورت‌های پروژه و ورکر
+import '../../features/payments/data/repositories_impl/payment_repository_impl.dart';
+import '../../features/payments/domain/repositories/payment_repository.dart';
+import '../../features/payments/domain/usecases/add_payment_usecase.dart';
 import '../../features/projects/data/repositories_impl/project_repository_impl.dart';
 import '../../features/projects/domain/repositories/project_repository.dart';
 import '../../features/projects/domain/usecases/add_project_usecase.dart';
+import '../../features/projects/domain/usecases/delete_project_usecase.dart';
+import '../../features/projects/domain/usecases/update_project_usecase.dart';
 import '../../features/projects/domain/usecases/watch_projects_usecase.dart';
+import '../../features/receipts/data/repositories_impl/receipt_repository_impl.dart';
+import '../../features/receipts/domain/repositories/receipt_repository.dart';
+import '../../features/receipts/domain/usecases/add_receipt_usecase.dart';
+import '../../features/settings/data/backup_repository.dart';
 import '../../features/workers/data/repositories_impl/worker_repository_impl.dart';
 import '../../features/workers/domain/repositories/worker_repository.dart'; // اینترفیس ورکر
 import '../../features/workers/domain/usecases/add_worker_usecase.dart';
+import '../../features/workers/domain/usecases/delete_worker_usecase.dart';
+import '../../features/workers/domain/usecases/update_worker_usecase.dart';
 import '../../features/workers/domain/usecases/watch_workers_usecase.dart';
 
 // ایمپورت‌های Work Entry (حتما اضافه شود)
@@ -32,7 +43,6 @@ WorkerRepository workerRepository(Ref ref) {
   return WorkerRepositoryImpl(db);
 }
 
-// 3. UseCases
 @riverpod
 WatchWorkersUseCase watchWorkersUseCase(Ref ref) {
   return WatchWorkersUseCase(ref.watch(workerRepositoryProvider));
@@ -41,6 +51,16 @@ WatchWorkersUseCase watchWorkersUseCase(Ref ref) {
 @riverpod
 AddWorkerUseCase addWorkerUseCase(Ref ref) {
   return AddWorkerUseCase(ref.watch(workerRepositoryProvider));
+}
+
+@riverpod
+UpdateWorkerUseCase updateWorkerUseCase(Ref ref) {
+  return UpdateWorkerUseCase(ref.watch(workerRepositoryProvider));
+}
+
+@riverpod
+DeleteWorkerUseCase deleteWorkerUseCase(Ref ref) {
+  return DeleteWorkerUseCase(ref.watch(workerRepositoryProvider));
 }
 
 // --- PROJECTS DI ---
@@ -61,6 +81,15 @@ AddProjectUseCase addProjectUseCase(Ref ref) {
   return AddProjectUseCase(ref.watch(projectRepositoryProvider));
 }
 
+@riverpod
+UpdateProjectUseCase updateProjectUseCase(Ref ref) {
+  return UpdateProjectUseCase(ref.watch(projectRepositoryProvider));
+}
+
+@riverpod
+DeleteProjectUseCase deleteProjectUseCase(Ref ref) {
+  return DeleteProjectUseCase(ref.watch(projectRepositoryProvider));
+}
 
 // --- WORK ENTRIES DI ---
 
@@ -76,3 +105,33 @@ AddWorkEntryUseCase addWorkEntryUseCase(Ref ref) {
   // اینجا باید repository مربوط به work entry پاس داده شود
   return AddWorkEntryUseCase(ref.watch(workEntryRepositoryProvider));
 }
+
+// --- PAYMENTS DI ---
+@riverpod
+PaymentRepository paymentRepository(Ref ref) {
+  return PaymentRepositoryImpl(ref.watch(appDatabaseProvider));
+}
+
+@riverpod
+AddPaymentUseCase addPaymentUseCase(Ref ref) {
+  return AddPaymentUseCase(ref.watch(paymentRepositoryProvider));
+}
+
+// --- RECEIPTS DI ---
+@riverpod
+ReceiptRepository receiptRepository(Ref ref) {
+  return ReceiptRepositoryImpl(ref.watch(appDatabaseProvider));
+}
+
+@riverpod
+AddReceiptUseCase addReceiptUseCase(Ref ref) {
+  return AddReceiptUseCase(ref.watch(receiptRepositoryProvider));
+}
+
+// --- SETTINGS & BACKUP DI ---
+
+@riverpod
+BackupRepository backupRepository(Ref ref) {
+  return BackupRepository(ref.watch(appDatabaseProvider));
+}
+
--- lib/core/di/dependency_injection.g.dart
+++ lib/core/di/dependency_injection.g.dart
@@ -6,7 +6,7 @@ part of 'dependency_injection.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$appDatabaseHash() => r'3d3a397d2ea952fc020fce0506793a5564e93530';
+String _$appDatabaseHash() => r'8c69eb46d45206533c176c88a926608e79ca927d';
 
 /// See also [appDatabase].
 @ProviderFor(appDatabase)
@@ -22,7 +22,7 @@ final appDatabaseProvider = Provider<AppDatabase>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef AppDatabaseRef = ProviderRef<AppDatabase>;
-String _$workerRepositoryHash() => r'd6ef47dca582f21d98fae37d84f9aec661d7fb0c';
+String _$workerRepositoryHash() => r'31042a3ef20aa77e8fab812011ef6fad079d78bd';
 
 /// See also [workerRepository].
 @ProviderFor(workerRepository)
@@ -40,7 +40,7 @@ final workerRepositoryProvider = AutoDisposeProvider<WorkerRepository>.internal(
 // ignore: unused_element
 typedef WorkerRepositoryRef = AutoDisposeProviderRef<WorkerRepository>;
 String _$watchWorkersUseCaseHash() =>
-    r'5943df13ae0367d2d6b02789cc914044f6dff961';
+    r'b5f8c1a8d3647a188180e7177f8f8b33e800b656';
 
 /// See also [watchWorkersUseCase].
 @ProviderFor(watchWorkersUseCase)
@@ -58,7 +58,7 @@ final watchWorkersUseCaseProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef WatchWorkersUseCaseRef = AutoDisposeProviderRef<WatchWorkersUseCase>;
-String _$addWorkerUseCaseHash() => r'3794025e2a6a238d7a2b1898e2e830269fd67a63';
+String _$addWorkerUseCaseHash() => r'bffee1c22d75a83d5248c2f24434e70b707a4576';
 
 /// See also [addWorkerUseCase].
 @ProviderFor(addWorkerUseCase)
@@ -75,7 +75,45 @@ final addWorkerUseCaseProvider = AutoDisposeProvider<AddWorkerUseCase>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef AddWorkerUseCaseRef = AutoDisposeProviderRef<AddWorkerUseCase>;
-String _$projectRepositoryHash() => r'45ab9cd9f7ae8bd733ff39124b724aae4d17f2ca';
+String _$updateWorkerUseCaseHash() =>
+    r'0273438d2f182af0ae7ab3f944824aa6a18e29c6';
+
+/// See also [updateWorkerUseCase].
+@ProviderFor(updateWorkerUseCase)
+final updateWorkerUseCaseProvider =
+    AutoDisposeProvider<UpdateWorkerUseCase>.internal(
+  updateWorkerUseCase,
+  name: r'updateWorkerUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$updateWorkerUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UpdateWorkerUseCaseRef = AutoDisposeProviderRef<UpdateWorkerUseCase>;
+String _$deleteWorkerUseCaseHash() =>
+    r'b598bdd74d7c36b407f335ab5d4162d411e47475';
+
+/// See also [deleteWorkerUseCase].
+@ProviderFor(deleteWorkerUseCase)
+final deleteWorkerUseCaseProvider =
+    AutoDisposeProvider<DeleteWorkerUseCase>.internal(
+  deleteWorkerUseCase,
+  name: r'deleteWorkerUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$deleteWorkerUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DeleteWorkerUseCaseRef = AutoDisposeProviderRef<DeleteWorkerUseCase>;
+String _$projectRepositoryHash() => r'e2cb8509c346b1fd65df3e31cd251a59250a3ae8';
 
 /// See also [projectRepository].
 @ProviderFor(projectRepository)
@@ -94,7 +132,7 @@ final projectRepositoryProvider =
 // ignore: unused_element
 typedef ProjectRepositoryRef = AutoDisposeProviderRef<ProjectRepository>;
 String _$watchProjectsUseCaseHash() =>
-    r'72e356ba17b982f687fd8279ef519a558e392cbb';
+    r'bc80dc8cb45111d1fa985e7d0b2923dc5d3063bf';
 
 /// See also [watchProjectsUseCase].
 @ProviderFor(watchProjectsUseCase)
@@ -112,7 +150,7 @@ final watchProjectsUseCaseProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef WatchProjectsUseCaseRef = AutoDisposeProviderRef<WatchProjectsUseCase>;
-String _$addProjectUseCaseHash() => r'7accce1502bdb8346c5f1861ebc5321ef874983e';
+String _$addProjectUseCaseHash() => r'686b97ce0f5d608c6eb5ad004c1c99130a5c5dcb';
 
 /// See also [addProjectUseCase].
 @ProviderFor(addProjectUseCase)
@@ -130,8 +168,46 @@ final addProjectUseCaseProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef AddProjectUseCaseRef = AutoDisposeProviderRef<AddProjectUseCase>;
+String _$updateProjectUseCaseHash() =>
+    r'1981a5dddd1b26d7184ef6488ad0b5e522ea2c44';
+
+/// See also [updateProjectUseCase].
+@ProviderFor(updateProjectUseCase)
+final updateProjectUseCaseProvider =
+    AutoDisposeProvider<UpdateProjectUseCase>.internal(
+  updateProjectUseCase,
+  name: r'updateProjectUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$updateProjectUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UpdateProjectUseCaseRef = AutoDisposeProviderRef<UpdateProjectUseCase>;
+String _$deleteProjectUseCaseHash() =>
+    r'6e416adb6b7cf0ce9fd96f5313799a28247895e0';
+
+/// See also [deleteProjectUseCase].
+@ProviderFor(deleteProjectUseCase)
+final deleteProjectUseCaseProvider =
+    AutoDisposeProvider<DeleteProjectUseCase>.internal(
+  deleteProjectUseCase,
+  name: r'deleteProjectUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$deleteProjectUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DeleteProjectUseCaseRef = AutoDisposeProviderRef<DeleteProjectUseCase>;
 String _$workEntryRepositoryHash() =>
-    r'10f0aaca4b9c162108de399940ef856f3f82c27b';
+    r'2e00ac5c3c20b67d13b2e07120059f2172b1ffe6';
 
 /// See also [workEntryRepository].
 @ProviderFor(workEntryRepository)
@@ -150,7 +226,7 @@ final workEntryRepositoryProvider =
 // ignore: unused_element
 typedef WorkEntryRepositoryRef = AutoDisposeProviderRef<WorkEntryRepository>;
 String _$addWorkEntryUseCaseHash() =>
-    r'567128308acf4693cc0c4921a678c2e1a6aa41bf';
+    r'b8b15ff36a654605d5ed4e1e8092546bad2a967b';
 
 /// See also [addWorkEntryUseCase].
 @ProviderFor(addWorkEntryUseCase)
@@ -168,5 +244,94 @@ final addWorkEntryUseCaseProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef AddWorkEntryUseCaseRef = AutoDisposeProviderRef<AddWorkEntryUseCase>;
+String _$paymentRepositoryHash() => r'1f03229f25af18d66fe2e7592620a82d8312396f';
+
+/// See also [paymentRepository].
+@ProviderFor(paymentRepository)
+final paymentRepositoryProvider =
+    AutoDisposeProvider<PaymentRepository>.internal(
+  paymentRepository,
+  name: r'paymentRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$paymentRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PaymentRepositoryRef = AutoDisposeProviderRef<PaymentRepository>;
+String _$addPaymentUseCaseHash() => r'94239cbd2d15c7ceaa9c9b5a3f1d0dc7b5482ea3';
+
+/// See also [addPaymentUseCase].
+@ProviderFor(addPaymentUseCase)
+final addPaymentUseCaseProvider =
+    AutoDisposeProvider<AddPaymentUseCase>.internal(
+  addPaymentUseCase,
+  name: r'addPaymentUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addPaymentUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AddPaymentUseCaseRef = AutoDisposeProviderRef<AddPaymentUseCase>;
+String _$receiptRepositoryHash() => r'721266a9f9b7051cff7f018b6bcfc10205b95d5b';
+
+/// See also [receiptRepository].
+@ProviderFor(receiptRepository)
+final receiptRepositoryProvider =
+    AutoDisposeProvider<ReceiptRepository>.internal(
+  receiptRepository,
+  name: r'receiptRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$receiptRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ReceiptRepositoryRef = AutoDisposeProviderRef<ReceiptRepository>;
+String _$addReceiptUseCaseHash() => r'84ecbb41caed73cfa860aa62cb2c3d3f84b3b38c';
+
+/// See also [addReceiptUseCase].
+@ProviderFor(addReceiptUseCase)
+final addReceiptUseCaseProvider =
+    AutoDisposeProvider<AddReceiptUseCase>.internal(
+  addReceiptUseCase,
+  name: r'addReceiptUseCaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addReceiptUseCaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AddReceiptUseCaseRef = AutoDisposeProviderRef<AddReceiptUseCase>;
+String _$backupRepositoryHash() => r'461f41fb27aee9e08a6728c852acdb7c6d647726';
+
+/// See also [backupRepository].
+@ProviderFor(backupRepository)
+final backupRepositoryProvider = AutoDisposeProvider<BackupRepository>.internal(
+  backupRepository,
+  name: r'backupRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$backupRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef BackupRepositoryRef = AutoDisposeProviderRef<BackupRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
